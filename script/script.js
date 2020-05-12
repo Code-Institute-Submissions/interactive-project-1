41f46d38efa9c3826b67bc1170ce8783d56f3cf5
Nathanael Guerrero <nateg@Nathanaels-MacBook-Pro.local>
2020-05-12 12:11:27 -0400

    change weather API

--- script/script.js
+++ script/script.js
@@ -1,41 +1,53 @@
 window.addEventListener('load', ()=> {
-    let lat;
-    let lng;
-    let currentLocation = document.querySelector(".currentLocation")
-    let value1 = document.querySelector(".value1")
-    let value2 = document.querySelector(".value2")
-    let value3 = document.querySelector(".value3")
-    let value4 = document.querySelector(".value4")
+    let appId = "a432cb2f329751921e3a076a4c5d6abc"
+    let units = "imperial"
+    let searchMethod;
 
-    if(navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition(position => {
-            lat  = position.coords.latitude;
-            lng  = position.coords.longitude;
-            
-            //Used "heroku" to bypass the DarkSky Weather api CORS
-            const heroku = 'https://cors-anywhere.herokuapp.com/';
-            const key = `${heroku}https://api.darksky.net/forecast/5d9e41a0749e684a9fc7edf31da2e543/${lat},${lng}`;
+    function getSearchMethod(searchTerm) {
+        if(searchTerm.length === 5 && Number.parseInt(searchTerm) + '' === searchTerm)
+            searchMethod = 'zip';
+        else
+            searchMethod = 'q';
+    }
 
-            fetch(key)
-            .then(response => {
-                return response.json();
-            })
-            .then(data => {
-                const {windSpeed, temperature, summary, humidity, icon} = data.currently;
-                currentLocation.textContent = data.timezone;
-                value1.textContent = windSpeed;
-                value2.textContent = temperature;
-                value3.textContent = summary;
-                value4.textContent = humidity;
-                setIcons(icon, document.querySelector('.icon'))
-            })
-            //console.log(key)
-        });
+    function searchWeather(searchTerm) {
+        getSearchMethod(searchTerm);
+        fetch(`http://api.openweathermap.org/data/2.5/weather?${searchMethod}=${searchTerm}&APPID=${appId}&units=${units}`).then(result => {
+            return result.json();
+        }).then(result => {
+            init(result);
+        })
     }
-    function setIcons(icon, iconID){
-        const skycons = new Skycons({color: "lightblue"});
-        const currentIcon = icon.replace(/-/g, "_").toUpperCase();
-        skycons.play();
-        return skycons.set(iconID, Skycons[currentIcon]);
+
+    function init(resultFromServer) {
+
+        let tempElement = document.getElementById('temperature');
+        let minTempElement = document.getElementById('min-temp');
+        let maxTempElement = document.getElementById('max-temp');
+        let pressureElement = document.getElementById('pressure');
+        let humidityElement = document.getElementById('humidity');
+        let cityNameElement = document.getElementById('cityName');
+        let weatherIcon = document.getElementById('iconImage');
+        let weatherDescription = document.getElementById('weatherDescription');
+
+        cityNameElement.innerHTML = resultFromServer.name;
+
+        weatherIcon.src = 'http://openweathermap.org/img/w/' + resultFromServer.weather[0].icon + '.png';
+
+        let resultDescription = resultFromServer.weather[0].description;
+        weatherDescription.innerText = resultDescription.charAt(0).toUpperCase() + resultDescription.slice(1);
+
+        tempElement.innerHTML = Math.floor(resultFromServer.main.temp) + '&#176F';
+        minTempElement.innerHTML = Math.floor(resultFromServer.main.temp_min) + '&#176F';
+        maxTempElement.innerHTML = Math.floor(resultFromServer.main.temp_max) + '&#176F';
+        pressureElement.innerHTML = resultFromServer.main.pressure + ' PA';
+        humidityElement.innerHTML = resultFromServer.main.humidity + '%';
     }
+
+    document.getElementById('searchBtn').addEventListener('click', () => {
+        let searchTerm = document.getElementById('searchInput').value;
+        if(searchTerm)
+            searchWeather(searchTerm);
+    });
+
 });
